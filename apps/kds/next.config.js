866af5598f756e5042cc8be4a90943141a8d2568
nicombing <nicombing@gmail.com>
2026-07-03 13:17:53 +0700

    fix: remove next-pwa wrapper from both apps to fix Vercel SSG crash

--- apps/kds/next.config.js
+++ apps/kds/next.config.js
@@ -1,11 +1,7 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public'
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@dayunnasgor/ui", "@dayunnasgor/schema"],
   output: "standalone"
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
